ff14b14b41c869107e694f69e8744fb58400070f
jiangtian <jiangtian@zpy360.com>
2020-05-02 17:17:52 +0800

    thinkphp6-arrayhelper

---
+++ src/InvalidConfigException.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * Author: jt
+ * Date: 2020/5/2
+ * Time: ÏÂÎç4:30
+ * E-Mail: 284053253@qq.com
+ * QQ: 284053253
+ */
+
+namespace JArray;
+
+/**
+ * InvalidConfigException represents an exception caused by incorrect object configuration.
+ */
+class InvalidConfigException extends \BadMethodCallException
+{
+	/**
+	 * @return string the user-friendly name of this exception
+	 */
+	public function getName()
+	{
+		return 'Invalid Configuration';
+	}
+}
---
+++ src/UnsetArrayValue.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * Author: jt
+ * Date: 2020/5/2
+ * Time: 下午4:13
+ * E-Mail: 284053253@qq.com
+ * QQ: 284053253
+ */
+
+namespace JArray;
+
+/**
+ * Object that represents the removal of array value while performing [[ArrayHelper::merge()]].
+ *
+ * Usage example:
+ *
+ * ```php
+ * $array1 = [
+ *     'ids' => [
+ *         1,
+ *     ],
+ *     'validDomains' => [
+ *         'example.com',
+ *         'www.example.com',
+ *     ],
+ * ];
+ *
+ * $array2 = [
+ *     'ids' => [
+ *         2,
+ *     ],
+ *     'validDomains' => new \yii\helpers\UnsetArrayValue(),
+ * ];
+ *
+ * $result = \yii\helpers\ArrayHelper::merge($array1, $array2);
+ * ```
+ *
+ * The result will be
+ *
+ * ```php
+ * [
+ *     'ids' => [
+ *         1,
+ *         2,
+ *     ],
+ * ]
+ * ```
+ */
+class UnsetArrayValue
+{
+	/**
+	 * Restores class state after using `var_export()`.
+	 *
+	 * @param array $state
+	 * @return UnsetArrayValue
+	 * @see var_export()
+	 * @since 2.0.16
+	 */
+	public static function __set_state($state)
+	{
+		return new self();
+	}
+}
